60c99c2c0f3bccaf7f88bec66e03a93c3ee0f081
chuc <chuccv2@fsoft.com.vn>
2020-03-05 19:35:18 +0700

    update 2

--- php2/index.php
+++ php2/index.php
@@ -34,10 +34,70 @@
     //     echo  $i;
     // }
 
-    $mang = array(1,2,3,4,5,7);
-    foreach ($mang as  $value) {
-      echo $value;
-    }
+    // $mang = array(1, 2, 3, 4, 5, 7);
+    // foreach ($mang as  $value) {
+    //     echo $value;
+    // }
+
+
+    // $mangAssosc = array(
+
+    //     'fullname'=>'chuc',
+    //     'age'=> '23'
+    // );
+
+    // foreach ($mangAssosc as $key => $value) {
+    //     echo $key." ".$value."<br>";
+    // }
+
+
+    //nang 2 chieuf
+
+    // $mang2d = array(
+    //     array(1, 2, 3, 4),
+    //     array(1, 2, 3, 5)
+    // );
+
+    // $mang3d = array(
+    //     array(1, 2, 3, 4),
+    //     array(1, 2, 3, 5),
+    //     array(2, 4, 5, 5)
+    // );
+
+    // foreach ($mang2d as $value) {
+    //     foreach ($value as  $val) {
+    //         echo $val.' ';
+    //     }
+    //     echo '<br>';
+    // }
+
+    // $mang2dAssoc = array(
+
+    //     array(
+    //         'name' => 'chuc',
+    //         'age ' => '23'
+    //     ),
+    //     array(
+    //         'ane' => 'd',
+    //         'da' => 'da'
+    //     )
+    // );
+    // //in mang 2 chiue lien hop
+    //  echo '<br>';
+    // foreach ($mang2dAssoc as $value) {
+
+    //     foreach ($value as $key => $val) {
+    //         echo $key . ' ' . $val . '<br>';
+    //     }
+    // }
+
+
+
+    //String 
+
+    $chuoi='chuoi';
+    echo strpos($chuoi,'i').'<br>';
+    echo strlen($chuoi);
     ?>
 </body>
 
